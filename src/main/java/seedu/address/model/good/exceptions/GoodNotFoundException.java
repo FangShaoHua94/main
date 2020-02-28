ac9f204bc9151a82416fe8a7bd4009fe23560af2
Daythehangedman <59592689+Daythehangedman@users.noreply.github.com>
2020-02-28 17:08:39 +0800

    add <UniqueGoodList> class and relevant tests.

---
+++ src/main/java/seedu/address/model/good/exceptions/GoodNotFoundException.java
@@ -0,0 +1,7 @@
+package seedu.address.model.good.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified good.
+ */
+public class GoodNotFoundException extends RuntimeException {
+}
